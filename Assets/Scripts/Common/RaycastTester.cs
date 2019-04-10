795102ba64313400769ce51a066154a720910afc
Philip Stenmark <phiste-7@student.ltu.se>
2019-04-10 15:58:59 +0200

    Added Interactable interface and raycasting test

---
+++ Assets/Scripts/Common/RaycastTester.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTester : MonoBehaviour
+{
+    [Range(1.0f, 10.0f)]
+    public float Lookahead = 4.0f;
+
+    private Interactable last;
+    private RaycastHit hitInfo;
+
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out hitInfo, Lookahead))
+        {
+            Interactable next = hitInfo.transform.GetComponent<Interactable>();
+            if (next != last)
+            {
+                SendMessageTo(last, "OnRaycastExit");
+                SendMessageTo(next, "OnRaycastEnter");
+                last = next;
+            }
+        }
+        else
+        {
+            SendMessageTo(last, "OnRaycastExit");
+            last = null;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && last != null)
+        {
+            last.OnInteract(gameObject);
+        }
+    }
+
+    private void SendMessageTo(Interactable target, string methodName)
+    {
+        if(target != null)
+        {
+            target.SendMessage(methodName);
+        }
+    }
+}
