9b9e61be80ad87cecdbf2f084dc717d959f53cf8
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-04-15 17:40:51 +0200

    Added minion statemachine and empty states
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

---
+++ Assets/Resources/Scripts/MinionStates.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingState : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Run()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+//-------------------------------------------------------------
+
+public class ReturnToSpawnerState : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Run()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+//-------------------------------------------------------------
+
+public class WanderState : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Run()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+//-------------------------------------------------------------
+
+public class SeekState : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Run()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
---
+++ Assets/Resources/Scripts/StateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public uint tickRate = 10;
+    public State CurrentState;
+    public GameObject EnemySpawner;
+    public GameObject TargetEntity;
+    public float AttackCooldown;
+    public uint AttackDamage;
+
+    //Pathfind component reference (pathFinder)
+
+    public void Start()
+    {
+        //pathFinder = getComponent.....
+    }
+
+    public void SetState(State newState)
+    {
+        if (CurrentState != null) CurrentState.Exit();
+        CurrentState = newState;
+        CurrentState.Enter();
+    }
+
+    public void Update()
+    {
+        if(System.Environment.TickCount % tickRate == 0)
+        {
+            if (CurrentState != null) CurrentState.Run();
+        }
+    }
+
+}
+
+//-------------------------------------------------------------
+
+public abstract class State
+{
+    public abstract void Enter();
+
+    public abstract void Run();
+
+    public abstract void Exit();
+}
+
