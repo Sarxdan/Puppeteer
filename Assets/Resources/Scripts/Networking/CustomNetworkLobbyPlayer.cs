e772a35def3769fe8f0b9b13cbd6d9086fe1c75b
Filip Renman <filren-7@student.ltu.se>
2019-04-16 09:33:21 +0200

    Forgot to add the networking folder (:

---
+++ Assets/Resources/Scripts/Networking/CustomNetworkLobbyPlayer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Mirror;
+
+/*
+* AUTHOR:
+* Filip Renman, Kristoffer Lundgren
+*
+* DESCRIPTION:
+* This class is created when a player joins/creates a lobby for a multiplayer game.
+* The class stores variables that can be used later to manipulate the game. For example choose which character the player plays as.
+*
+* CODE REVIEWED BY:
+* 
+*
+* CONTRIBUTORS:
+*/
+
+
+public class CustomNetworkLobbyPlayer : NetworkLobbyPlayer
+{
+    [SyncVar]
+    public bool PlayerIsReady;
+
+    [Header("UI")]
+    public Button ReadyButton;
+    public Text ReadyText;
+
+    void Start()
+    {
+        base.Start();
+        DontDestroyOnLoad(this.gameObject);
+
+        gameObject.transform.position = new Vector3((Index * 200) - 400, 200, 0);
+
+        if (!isLocalPlayer)
+        {
+            ReadyButton.gameObject.SetActive(false);
+        }
+
+        ReadyButton.onClick.AddListener(delegate { CmdChangeReadyState2(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerIsReady)
+            ReadyText.text = "Ready";
+        else
+            ReadyText.text = "Not Ready";
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        NetworkLobbyManager lobby = NetworkManager.singleton as NetworkLobbyManager;
+
+        if (lobby != null && SceneManager.GetActiveScene().name == lobby.LobbyScene)
+        {
+            gameObject.transform.SetParent(GameObject.Find("Players").transform);
+        }
+    }
+
+    public override void OnClientEnterLobby()
+    {
+        base.OnClientEnterLobby();
+    }
+
+    [Command]
+    public void CmdChangeReadyState2()
+    {
+        PlayerIsReady = !PlayerIsReady;
+        CustomNetworkManager lobby = CustomNetworkManager.singleton as CustomNetworkManager;
+        lobby?.PlayerReadyStatusChanged();
+    }
+}
