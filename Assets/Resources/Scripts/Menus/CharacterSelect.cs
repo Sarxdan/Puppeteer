7689a3bbca3a89583ea57132f717561fe3d521f5
Filip Renman <filren-7@student.ltu.se>
2019-04-16 11:21:39 +0200

    Started working on character select

---
+++ Assets/Resources/Scripts/Menus/CharacterSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject[] SelectableCharacters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
