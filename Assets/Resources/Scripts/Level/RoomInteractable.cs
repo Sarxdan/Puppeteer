efe3de1d28bb66b472a998cd204e3e6a1e450d53
Anton Jonsson <jonano-7@student.ltu.se>
2019-04-17 13:44:23 +0200

    Grab tool now finds interactable rooms and calls pickup function when needed

---
+++ Assets/Resources/Scripts/Level/RoomInteractable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* AUTHOR:
+* Benjamin "Boris" Vesterlund, Anton "Knugen" Jonsson
+*
+* DESCRIPTION:
+* Script to determine what rooms should do when interacted with.
+*
+* CODE REVIEWED BY:
+* 
+*
+* CONTRIBUTORS:
+*/
+
+public class RoomInteractable : Interactable
+{
+	public bool CanBePickedUp = true;
+
+	public override void OnInteractBegin(GameObject interactor)
+	{
+		
+	}
+
+	public override void OnInteractEnd(GameObject interactor)
+	{
+
+	}
+}
