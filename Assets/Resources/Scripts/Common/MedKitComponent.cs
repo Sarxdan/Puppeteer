e4ecc35f2cf5557fc3e4ccb8438f159a590ceaa4
Kristoffer Lundgren <klu0711@gmail.com>
2019-04-17 12:44:18 +0200

    Finished the current state of the med kit component, and removed temporary test scenes

--- Assets/Resources/Scripts/Common/MedKitComponent.cs
+++ Assets/Resources/Scripts/Common/MedKitComponent.cs
@@ -1,29 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+*
+* AUTHOR:
+* Kristoffer Lundgren
+*
+* DESCRIPTION:
+* This component adds a medkit to a players inventoty if the player does not already have one.
+* 
+*
+* CODE REVIEWED BY:
+* Philip Stenmark
+* 
+*/
 
+//TODO: Network functionality, eg. remove the medkit after it is picked up.
 public class MedKitComponent : Interactable
 {
-    [Header("Heal Amount")]
-    public uint HealAmount;
-
+    //Override from Interactable component
     public override void OnInteractBegin(GameObject interactor)
     {
         
     }
-
+    //Overrides from Interactable component, adds the medkit to the players inventory.
     public override void OnInteractEnd(GameObject interactor)
     {   
         PlayerController playerController = interactor.GetComponent<PlayerController>();
-        if(playerController.HasMedkit)
-        {
-            Destroy(gameObject);
-            return;
-        }else
-        {
-            playerController.HasMedkit = true;
-            Destroy(gameObject);
 
-        }
+        if(!playerController.HasMedkit)
+            playerController.HasMedkit = true;
+
     }
 }
