cff6615dbe4a64efc9214dc51e1ed46aced74df2
Philip Stenmark <phiste-7@student.ltu.se>
2019-04-18 15:17:20 +0200

    Added powerup recharge script

---
+++ Assets/Resources/Scripts/Common/PowerupRecharge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupRecharge : Interactable
+{
+    public override void OnInteractBegin(GameObject interactor)
+    {
+        var power = interactor.GetComponent<PowerupBase>();
+
+        // attempt to pickup recharge
+        if (power != null && !power.Charged)
+        {
+            power.Charged = true;
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnInteractEnd(GameObject interactor)
+    {
+        // empty
+    }
+}
