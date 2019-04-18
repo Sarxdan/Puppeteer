05074d59865a35c9f9deff41a73cff50217288e0
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-04-18 23:38:05 +0200

    Worked on PathfinderComponent Pathfinding kinda works, but it sometimes gets the start position wrong. Have to fix
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

---
+++ Assets/Resources/Scripts/Level/NavMesh.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class meshFace : System.Object
+{
+    public int a, b, c = -1;
+    public Vector3 aPos, bPos, cPos = new Vector3();
+    public Vector3 Origin;
+    public meshFace(int a, int b, int c, Vector3[] positionRefs)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        this.aPos = positionRefs[a];
+        this.bPos = positionRefs[b];
+        this.cPos = positionRefs[c];
+    }
+
+    public bool hasVertex(int vertexIndex)
+    {
+        return this.a == vertexIndex || this.b == vertexIndex || this.c == vertexIndex;
+    }
+
+    public bool hasVertex(Vector3 vertexPos)
+    {
+        return this.aPos == vertexPos || this.bPos == vertexPos || this.cPos == vertexPos;
+    }
+
+    public int this[int i]
+    {
+        get { return i == 0 ? this.a : i == 1 ? this.b : i == 2 ? this.c : -1; }
+        set {} //This will unlikely be used
+    }
+}
+
+
+[ExecuteInEditMode]
+public class NavMesh : MonoBehaviour
+{
+    public Mesh inputMesh;
+    
+    //Cache
+    public Vector3[] vertices;
+    public meshFace[] faces;
+    public Vector3[] faceOrigins;
+
+    public bool bake;
+    public bool draw;
+    Quaternion rotation = Quaternion.Euler(-90, 0, 0);
+
+    private Mesh highlight;
+
+    private void OnDrawGizmos()
+    {
+        if (bake)
+        {
+            bake = false;
+
+            vertices = new Vector3[inputMesh.vertices.Length];
+
+            for(int i = 0; i < inputMesh.vertices.Length; i++)
+            {
+                vertices[i] = rotation * inputMesh.vertices[i];
+            }
+
+            faceOrigins = new Vector3[(int)(inputMesh.triangles.Length/3)];
+            faces = new meshFace[(int)(inputMesh.triangles.Length / 3)];
+
+            for(int i = 0; i < faces.Length; i++)
+            {
+                faces[i] = new meshFace(inputMesh.triangles[i*3], inputMesh.triangles[i * 3 + 1], inputMesh.triangles[i * 3 + 2], vertices); 
+                faceOrigins[i] = (vertices[faces[i].a] + vertices[faces[i].b] + vertices[faces[i].c])/3;
+            }
+            
+
+
+        }
+        if (draw)
+        {
+            if (inputMesh != null)
+            {
+                Gizmos.DrawWireMesh(inputMesh, transform.position, rotation);
+            }
+            if(highlight != null)
+            {
+                Gizmos.DrawMesh(highlight, transform.position);
+            }
+        }
+    }
+
+    
+
+    public int getFaceFromEdge(int A, int B, List<int> filter)
+    {
+        for(int i = 0; i < faces.Length; i++)
+        {
+            if (!filter.Contains(i))
+            {
+                if ((faces[i].a == A || faces[i].b == A || faces[i].c == A) &&
+                   (faces[i].a == B || faces[i].b == B || faces[i].c == B))
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getFaceFromPoint(Vector3 point)
+    {
+        List<int> filteredFaces = new List<int>();
+        int closestFace = 0;
+        for (int i = 0; i < 10; i++) {
+            closestFace = getClosestFace(point, filteredFaces);
+            if (PointWithinFace(point, faces[closestFace]))
+            {
+                Debug.Log("Found closest face!");
+                return closestFace;
+            }
+            else
+            {
+                filteredFaces.Add(closestFace);
+            }
+        }
+        Mesh faceMesh = new Mesh();
+        faceMesh.vertices = new Vector3[]{
+            faces[closestFace].aPos,
+            faces[closestFace].bPos,
+            faces[closestFace].cPos
+        };
+        faceMesh.triangles = new int[] { 0, 1, 2 };
+
+        faceMesh.normals = new Vector3[]
+        {
+            Vector3.up,
+            Vector3.up,
+            Vector3.up
+        };
+
+        highlight = faceMesh;
+
+        Debug.DrawRay(faceOrigins[closestFace] + transform.position, Vector3.up * 3, Color.blue, Time.deltaTime);
+        Debug.Log("Did not find closest face!");
+        return 0;
+    }
+
+    public int getClosestFace(Vector3 position, List<int> filter)
+    {
+        float closestDistance = Mathf.Infinity;
+        int closestFace = -1;
+        for (int i = 0; i < faceOrigins.Length; i++)
+        {
+            if (!filter.Contains(i))
+            {
+                float distance = Vector3.Distance(faceOrigins[i] + transform.position, position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestFace = i;
+                }
+            }
+        }
+
+
+        return closestFace;
+
+    }
+    
+
+    public bool PointWithinFace(Vector3 point, meshFace triangle)
+    {
+        return (
+                SameSideOf(point, triangle.aPos + transform.position, triangle.bPos + transform.position, triangle.cPos) &&
+                SameSideOf(point, triangle.bPos + transform.position, triangle.aPos + transform.position, triangle.cPos) &&
+                SameSideOf(point, triangle.cPos + transform.position, triangle.aPos + transform.position, triangle.bPos));
+    }
+
+    public bool SameSideOf(Vector3 p, Vector3 C, Vector3 A, Vector3 B)
+    {
+        //Returns whether pA and pB are on the same side of the line AB or not
+        Vector3 crossA = Vector3.Cross(B - A, p - A);
+        Vector3 crossB = Vector3.Cross(B - A, C - A);
+        return Vector3.Dot(crossA,crossB) >= 0;
+    }
+
+    private RaycastHit[] recursiveRaycast(Ray ray, float length, int layerMask)
+    {
+       
+        return performRecursiveRaycast(new List<RaycastHit>(), ray, length, layerMask).ToArray();
+    }
+
+    private List<RaycastHit> performRecursiveRaycast(List<RaycastHit> hitList, Ray ray, float lengthRemaining, int layerMask)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, lengthRemaining))
+        {
+            lengthRemaining -= hit.distance;
+            hitList.Add(hit);
+            Ray nextRay = new Ray(hit.point + ray.direction*.01f, ray.direction);
+            return performRecursiveRaycast(hitList, nextRay, lengthRemaining, layerMask);
+        }
+        else
+        {
+            return hitList;
+        }
+        
+    }
+}
--- Assets/Resources/Scripts/Minion/PathfinderComponent.cs
+++ Assets/Resources/Scripts/Minion/PathfinderComponent.cs
@@ -4,65 +4,320 @@ using UnityEngine;
 
 class AStarNode
 {
-    public float gCost;
-    public AStarNode(float gCost, int meshIndex)
+    public float FCost;
+    public int FaceIndex;
+    public meshFace Face;
+    public AStarNode Parent;
+    public AStarNode(float fCost, int faceIndex, meshFace Face, AStarNode parent)
     {
-        this.gCost = gCost;
+        this.FCost = fCost;
+        this.FaceIndex = faceIndex;
+        this.Parent = parent;
+        this.Face = Face;
     }
 }
 
-[ExecuteInEditMode]
 public class PathfinderComponent : MonoBehaviour
 {
     public float MovementSpeed = 1;
-    List<Vector3> path;
+    public int MaxRecursionDepth = 20;
+    public List<Vector3> path;
+    private Transform navmeshTransform;
+    private bool IsPathfinding = false;
+
+    public float LegHeight;
     // Start is called before the first frame update
+
+    private Rigidbody rigidBody;
     void Start()
     {
-        
+        this.rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
+        
+        if(IsPathfinding){
+        for(int i = 0; i < this.path.Count-1; i++){
+            Debug.DrawLine(this.path[i] + transform.up, this.path[i+1] + transform.up, Color.black, Time.deltaTime);
+        }
+            move();
+        }
+
+    }
+
+    private void createOrUpdateNode(int faceIndex, meshFace face, Vector3 position, Vector3 start, Vector3 end, Dictionary<int, AStarNode> unEvaluatedNodes, List<int> faceFilter, AStarNode parent)
+    {
+        unEvaluatedNodes[faceIndex] = new AStarNode(Vector3.Distance(start, position) + Vector3.Distance(position,end),faceIndex, face, parent);
+        faceFilter.Add(faceIndex);
+    }
+
+    private void move(){
+        if(path.Count <= 0){
+            IsPathfinding = false;
+            return;
+        }
+        Vector3 deltaPos = path[0] - transform.position + transform.up * LegHeight + navmeshTransform.position;
+        float distance = 0;
+        if(deltaPos.magnitude < this.MovementSpeed * Time.deltaTime){
+            distance = deltaPos.magnitude;
+            this.path.RemoveAt(0);
+        }else{
+            distance = this.MovementSpeed * Time.deltaTime;
+        }
+        rigidBody.MovePosition(transform.position + deltaPos.normalized * this.MovementSpeed * Time.deltaTime );
+    }
+
+    public void MoveTo(Vector3 end, NavMesh navmesh)
+    {
+        
+        RaycastHit hit;
+        Vector3 start;
         if (Physics.Raycast(transform.position, -transform.up, out hit))
         {
             NavMesh navMesh = hit.transform.GetComponent<NavMesh>();
             if (navMesh != null)
             {
-                Debug.DrawRay(transform.position, Vector3.up * 10, Color.green, 1);
-                navMesh.getFaceFromPoint(hit.point);
+                start = hit.point;
+                navmeshTransform = navMesh.transform;
+            }else{
+                return;
             }
+        }else{
+            return;
         }
-    }
 
-    void MoveTo(Vector3 position)
-    {
-        int startFace = 0;
-        Vector3 end = new Vector3(10,0,10);
+        //Vector3 end = new Vector3(-10,0,-10);
 
-        List<AStarNode> unEvaluatedNodes = new List<AStarNode>();
-        List<AStarNode> evaluatedNodes = new List<AStarNode>();
+        Dictionary<int,AStarNode> unEvaluatedNodes = new Dictionary<int, AStarNode>();
+        Dictionary<int, AStarNode> evaluatedNodes = new Dictionary<int, AStarNode>();
+        List<int> faceFilter = new List<int>();
         List<AStarNode> facePath = new List<AStarNode>();
+        
+        this.path = new List<Vector3>();
 
+        Debug.DrawRay(start, Vector3.up * 5, Color.red, 10);
+        Debug.DrawRay(end, Vector3.up * 5, Color.green, 10);
 
+        int startFaceIndex = navmesh.getFaceFromPoint(start);
 
-        while (true)
+        createOrUpdateNode(startFaceIndex, navmesh.faces[startFaceIndex], navmesh.faceOrigins[startFaceIndex], end, start, unEvaluatedNodes, faceFilter, null);
+
+        AStarNode currentNode = null;
+
+        while (unEvaluatedNodes.Count > 0)
         {
-            AStarNode currentNode = null;
-            float lowestGCost = Mathf.Infinity;
-            foreach(AStarNode node in unEvaluatedNodes)
+            //Fetches unevaluated node with lowest FCost
+            float lowestFCost = Mathf.Infinity;
+            foreach(AStarNode node in unEvaluatedNodes.Values)
             {
-                if(node.gCost < lowestGCost)
+                if(node.FCost < lowestFCost)
                 {
-                    lowestGCost = node.gCost;
+                    lowestFCost = node.FCost;
                     currentNode = node;
                 }
             }
-            unEvaluatedNodes.Remove(currentNode);
-            evaluatedNodes.Add(currentNode);
+
+            //Moves node to evaluated list
+            unEvaluatedNodes.Remove(currentNode.FaceIndex);
+            evaluatedNodes[currentNode.FaceIndex] = currentNode;
+
+            //Check if current face is destination
+            if (navmesh.PointWithinFace(end, navmesh.faces[currentNode.FaceIndex])) break;
+
+            //Fetches adjacent faces, filtering already fetched faces
+            int newFace1 = navmesh.getFaceFromEdge(currentNode.Face.a, currentNode.Face.b, faceFilter);
+            int newFace2 = navmesh.getFaceFromEdge(currentNode.Face.a, currentNode.Face.c, faceFilter);
+            int newFace3 = navmesh.getFaceFromEdge(currentNode.Face.b, currentNode.Face.c, faceFilter);
+
+            //If edge has a face, add it as a A* node
+            if (newFace1 != -1)
+            {
+                createOrUpdateNode(newFace1, navmesh.faces[newFace1], navmesh.faceOrigins[newFace1], end, start, unEvaluatedNodes, faceFilter, currentNode);
+            }
+
+            if (newFace2 != -1)
+            {
+                createOrUpdateNode(newFace2, navmesh.faces[newFace2], navmesh.faceOrigins[newFace2], end, start, unEvaluatedNodes, faceFilter, currentNode);
+            }
+
+            if (newFace3 != -1)
+            {
+                createOrUpdateNode(newFace3, navmesh.faces[newFace3], navmesh.faceOrigins[newFace3], end, start, unEvaluatedNodes, faceFilter, currentNode);
+            }
+            
+
+        }
+        this.path.Add(start);
+        this.path.AddRange(funnelPathfind(currentNode, start, new List<Vector3>(), 0));
+
+        this.path.Add(end);
+        for(int i = 0; i < this.path.Count-1; i++){
+            Debug.DrawLine(this.path[i] + transform.up, this.path[i+1] + transform.up, Color.black, Time.deltaTime);
         }
 
+        
+        IsPathfinding = true;
     }
+
+    private List<Vector3> funnelPathfind(AStarNode currentNode, Vector3 origin, List<Vector3> nodeList, int recursionIndex){
+        recursionIndex ++;
+        if(recursionIndex>MaxRecursionDepth || currentNode.Parent == null) return nodeList;
+
+        float debugLineTime = 5;
+        Debug.DrawRay(origin, -Vector3.up, Color.yellow, debugLineTime);
+        Vector3 lastRight = getRight(currentNode, origin);
+        Vector3 lastLeft = getLeft(currentNode, origin);
+        Vector3 right = lastRight;
+        Vector3 left = lastLeft;
+
+        AStarNode lastRightNode = currentNode.Parent;
+        AStarNode lastLeftNode = currentNode.Parent;
+        Debug.DrawRay(lastRight, Vector3.up, Color.red, debugLineTime);
+        Debug.DrawRay(lastLeft, -Vector3.up, Color.green, debugLineTime);
+
+        currentNode = currentNode.Parent;
+        //Backtraces through parents and adds faces to facepath list
+        while (currentNode.Parent != null)
+        {
+            AStarNode nextNode = currentNode.Parent;
+            Debug.DrawRay(currentNode.Face.Origin, Vector3.up*4, Color.cyan, debugLineTime);
+            
+            Vector3 newVert = new Vector3();
+
+            //If next node does not contain current right, 
+            if (!nextNode.Face.hasVertex(right))
+            {
+                newVert = getRight(nextNode, left);
+                right = newVert;
+                Debug.DrawRay(newVert, Vector3.up, Color.red,debugLineTime);
+
+                Vector3 leftRightCross = Vector3.Cross(newVert - origin, lastLeft - origin);
+                float leftRightDot = Vector3.Dot(leftRightCross, Vector3.up);
+
+                if (leftRightDot > 0.0f)
+                {
+                    nodeList.Insert(0,lastLeft);
+                    return funnelPathfind(getLastOccurance(lastLeftNode,lastLeft), lastLeft, nodeList, recursionIndex);
+                }
+                
+                Vector3 newOldCross = Vector3.Cross(newVert - origin, lastRight - origin);
+                float newOldDot = Vector3.Dot(newOldCross, Vector3.up);
+
+                if(newOldDot > 0){
+                    lastRight = newVert;
+                    lastRightNode = nextNode;
+                    if(currentNode.Face.hasVertex(lastLeft))lastLeftNode = nextNode;
+                }else{
+                    Debug.DrawLine(lastRight + Vector3.up, origin, Color.red, debugLineTime);
+                    Debug.DrawLine(newVert + Vector3.up, origin, Color.yellow, debugLineTime);
+                }
+            }
+            else
+            {
+                newVert = getLeft(nextNode, right);
+                left = newVert;
+                Debug.DrawRay(newVert, Vector3.up, Color.green, debugLineTime);
+
+                Vector3 leftRightCross = Vector3.Cross(lastRight - origin, newVert - origin);
+                float leftRightDot = Vector3.Dot(leftRightCross, Vector3.up);
+
+                if (leftRightDot > 0.0f)
+                {
+                    nodeList.Insert(0,lastRight);
+                    return funnelPathfind(getLastOccurance(lastRightNode,lastRight), lastRight, nodeList, recursionIndex);
+                }
+
+                Vector3 newOldCross = Vector3.Cross(lastLeft - origin, newVert - origin);
+                float newOldDot = Vector3.Dot(newOldCross, Vector3.up);
+
+                if(newOldDot > 0){
+                    lastLeft = newVert;
+                    lastLeftNode = nextNode;
+                    if(currentNode.Face.hasVertex(lastRight))lastRightNode = nextNode;
+                }else{
+                    //Debug.DrawLine(lastLeft + Vector3.up, origin, Color.green, debugLineTime);
+                    Debug.DrawLine(newVert + Vector3.up, origin, Color.yellow, debugLineTime);
+                }
+            }
+            currentNode = nextNode;
+            
+
+        }
+
+        return nodeList;
+    }
+
+    private List<T> listSum<T>(List<T> A, List<T> B)
+    {
+        List<T> ret = new List<T>();
+        ret.AddRange(A);
+        ret.AddRange(B);
+        return ret;
+    }
+
+    private Vector3 getRight(AStarNode node, Vector3 origin)
+    {
+        return getLeftOrRight(node, origin, 1);
+    }
+    private Vector3 getLeft(AStarNode node, Vector3 origin)
+    {
+        return getLeftOrRight(node, origin, -1);
+    }
+
+    private AStarNode getLastOccurance(AStarNode node, Vector3 point){
+        if(node.Parent != null && node.Parent.Face.hasVertex(point)){
+            return getLastOccurance(node.Parent, point);
+        }else{
+            return node;
+        }
+    }
+    private Vector3 getLeftOrRight(AStarNode node, Vector3 origin, int inverse)
+    {
+        //If someone knows how to do this in one line plz call me ;*
+        Vector3 A = new Vector3();
+        Vector3 B = new Vector3();
+        Vector3 C = new Vector3();
+
+
+        if(origin == node.Face.aPos)
+        {
+            A = node.Face.aPos;
+            B = node.Face.bPos;
+            C = node.Face.cPos;
+        }
+        else if(origin == node.Face.bPos)
+        {
+            A = node.Face.bPos;
+            B = node.Face.cPos;
+            C = node.Face.aPos;
+        }
+        else if(origin == node.Face.cPos)
+        {
+            A = node.Face.cPos;
+            B = node.Face.bPos;
+            C = node.Face.aPos;
+        }else{
+            
+            return getLeftOrRight(node, getLonelyNode(node.Face, node.Parent.Face), inverse);
+        }
+
+        Vector3 normal = Vector3.Cross(B-A, C-A);
+        float dot = Vector3.Dot(normal, transform.up);
+        Vector3 DB = Vector3.Normalize(C - (B+C)/2);
+
+        return dot * inverse > 0 ? C : B;
+    }
+
+
+    private Vector3 getLonelyNode(meshFace A, meshFace B)
+    {
+        if (!B.hasVertex(A.a)) return A.aPos;
+        if (!B.hasVertex(A.b)) return A.bPos;
+        if (!B.hasVertex(A.c)) return A.cPos;
+        return new Vector3();
+    }
+    
 }
