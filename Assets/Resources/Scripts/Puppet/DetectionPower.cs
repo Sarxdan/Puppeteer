bc35f9b95f8eb305da7cf32b70ea4ff56da73ce4
Philip Stenmark <phiste-7@student.ltu.se>
2019-04-23 11:25:41 +0200

    Added classes for all powerups

---
+++ Assets/Resources/Scripts/Puppet/DetectionPower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionPower : PowerupBase
+{
+    public override void OnActivate()
+    {
+        
+    }
+
+    public override void OnComplete()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/Puppet/InvisibilityPower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibilityPower : PowerupBase
+{
+    public override void OnActivate()
+    {
+        
+    }
+
+    public override void OnComplete()
+    {
+        
+    }
+}
