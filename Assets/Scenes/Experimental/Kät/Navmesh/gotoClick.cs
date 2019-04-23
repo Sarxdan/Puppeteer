8dc05373705db4db9d8b3fafb3257049856c8bc1
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-04-23 11:16:01 +0200

    Added more navmesh resources Changed gitignore (blendercam and fbxexporter)
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

---
+++ Assets/Scenes/Experimental/Kät/Navmesh/gotoClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class gotoClick : MonoBehaviour
+{
+    PathfinderComponent agent;
+    void Start()
+    {
+        agent = transform.GetComponent<PathfinderComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Fire") > 0)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit) && hit.transform.CompareTag("RoomCollider"))
+            {
+                agent.MoveTo(hit.point, hit.transform.GetComponent<NavMesh>());
+            }
+        }
+    }
+}
