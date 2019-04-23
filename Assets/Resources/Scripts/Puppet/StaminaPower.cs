4de54755a4b2da05e6775158dd2111ef601f6050
Philip Stenmark <phiste-7@student.ltu.se>
2019-04-23 10:34:32 +0200

    Added getter for retrieving percentage left of powerup

---
+++ Assets/Resources/Scripts/Puppet/StaminaPower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaPower : PowerupBase
+{
+    public float SpeedModifier = 2.0f;
+
+    public override void OnActivate()
+    {
+        
+    }
+
+    public override void OnComplete()
+    {
+        
+    }
+}
