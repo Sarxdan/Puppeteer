1c2d7e4354b90d5fabddf734402b9a699291bd62
Philip Stenmark <phiste-7@student.ltu.se>
2019-04-23 11:35:47 +0200

    Added header description

--- Assets/Resources/Scripts/Puppet/InvisibilityPower.cs
+++ Assets/Resources/Scripts/Puppet/InvisibilityPower.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * AUTHOR:
+ * Philip Stenmark
+ * 
+ * DESCRIPTION:
+ * When activated, the owner will become invisible for all observers except other players.
+ * 
+ * CODE REVIEWED BY:
+ * 
+ */
 public class InvisibilityPower : PowerupBase
 {
     public override void OnActivate()
