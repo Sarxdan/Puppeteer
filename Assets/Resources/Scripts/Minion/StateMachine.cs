5345322c6176816fcc8e671229be9fa0b4be5a57
Carl Appelkvist <c.appelkvist@hotmail.com>
2019-04-23 17:44:58 +0200

    Minion states

--- Assets/Resources/Scripts/Minion/StateMachine.cs
+++ Assets/Resources/Scripts/Minion/StateMachine.cs
@@ -1,6 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MinionStates;
+/*
+ * AUTHOR:
+ * Ludvig Björk Förare
+ * Carl Appelkvist
+ * 
+ * DESCRIPTION:
+ * 
+ * 
+ * CODE REVIEWED BY:
+ * 
+ */
+
 
 public class StateMachine : MonoBehaviour
 {
@@ -10,15 +23,13 @@ public class StateMachine : MonoBehaviour
     public GameObject TargetEntity;
     public float AttackCooldown;
     public uint AttackDamage;
-
-
-
+    public bool AttackRdy;
+    public List<GameObject> DownedPuppets;
     //Pathfind component reference (pathFinder)
 
     public void Start()
     {
-
-        //pathFinder = getComponent.....
+        SetState(new AttackingState(this));
     }
 
     public void SetState(State newState)
@@ -30,17 +41,28 @@ public class StateMachine : MonoBehaviour
 
     public void Update()
     {
-        
         if (System.Environment.TickCount % tickRate == 0)
         {
             if (CurrentState != null) CurrentState.Run();
         }
+
     }
+
+    //IEnumerator hitRoutine()
+    //{
+    //    Debug.Log(AttackRdy);
+    //    AttackRdy = true;
+    //    yield return new WaitForSecondsRealtime(5);
+    //    StopCoroutine("waitRoutine");
+
+    //}
 }
+
+
 
 //-------------------------------------------------------------
 
-public abstract class State : MonoBehaviour
+public abstract class State
 {
     public abstract void Enter();
 
