6985befa59e13bb4187673553d8c6689bbcead75
Kristoffer Lundgren <klu0711@gmail.com>
2019-04-24 14:35:47 +0200

    working un puppet ui

--- Assets/Resources/Scripts/Puppet/HUDScript.cs
+++ Assets/Resources/Scripts/Puppet/HUDScript.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDScript : MonoBehaviour
 {
 
+    // The player whose health is to me monitored
+    public Transform Owner;
     // The actual health "bar"
     public RectTransform HealthBarFill;
     // The med kit icon
     public RectTransform MedKit;
-    // The player whose health is to me monitored
-    public Transform Owner;
+    // The mask that shows how much is left on the power up duration
+    public RectTransform PowerUpFill;
+    // The text for the current ammo
+    public Text CurrentAmmo;
     //The health component to monitor
     private HealthComponent healthComponent;
     // Used to check is player has a med kit
     private PlayerController playerController;
 
-    #region Health UI variables
+    private PowerupBase powerUp;
+
     // Current health of the player
     private uint health;
     // The max health of the player
@@ -25,11 +31,13 @@ public class HUDScript : MonoBehaviour
     private float xScale;
     // The lerp amount per update
     private float increment = 0;
+    // Used for checking if the hp has changed
     private uint previousHP;
     // The hp value to lerp to, changes when the hp is modified
     private uint lerpTo;
-    private float healthBarSpeed = 0.8f;
-    #endregion
+    // Controls how fast the health bar changes
+    public float HealthBarSpeed = 0.8f;
+
 
     private bool medKitToggle = true;
 
@@ -38,6 +46,7 @@ public class HUDScript : MonoBehaviour
     {
         healthComponent = Owner.GetComponent<HealthComponent>();    
         playerController = Owner.GetComponent<PlayerController>();
+        powerUp = Owner.GetComponent<PowerupBase>();
         xScale = HealthBarFill.localScale.x;
         previousHP = healthComponent.Health;
     }   
@@ -46,6 +55,9 @@ public class HUDScript : MonoBehaviour
     void Update()
     {
         drawHealthBar();
+        int duration = powerUp.Duration;
+        float delta = powerUp.PercentageLeft;
+        PowerUpFill.localScale = new Vector3(1, 1.0f-Mathf.Lerp(0.0f, 1, delta), 1);
 
         if(playerController.HasMedkit && medKitToggle)
         {
@@ -58,6 +70,23 @@ public class HUDScript : MonoBehaviour
             medKitToggle = true;
         }
         
+
+        if(playerController.CurrentWeapon != null)
+        {
+            float magazines = playerController.Ammunition / playerController.CurrentWeapon.GetComponent<WeaponComponent>().Capacity; 
+            
+            if(magazines > 100)
+            {
+                CurrentAmmo.text = Mathf.FloorToInt(magazines).ToString();
+            }
+            if(magazines > 10)
+            {
+                CurrentAmmo.text = "00" + Mathf.FloorToInt(magazines).ToString();
+            }else
+            {
+                CurrentAmmo.text = "0" +  Mathf.FloorToInt(magazines).ToString();
+            }
+        }
 
     }
 
@@ -76,7 +105,7 @@ public class HUDScript : MonoBehaviour
         // Scale the health bar
         HealthBarFill.localScale = new Vector3(Mathf.Lerp(xScale * (Mathf.Clamp(lerpTo, 0, maxHealth)/maxHealth), xScale * (Mathf.Clamp(health, 0, maxHealth)/maxHealth), increment), 0.1f, 1.0f);
 
-        increment += healthBarSpeed * Time.deltaTime;
+        increment += HealthBarSpeed * Time.deltaTime;
         maxHealth = healthComponent.MaxHealth;
         if(increment >= 1)
         {
