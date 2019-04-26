057307425043eb357e8b7bbd50e759de34be3256
Anton Jonsson <jonano-7@student.ltu.se>
2019-04-26 13:03:31 +0200

    Added script to startroom

---
+++ Assets/Resources/Scripts/Level/StartRoomScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRoomScript : MonoBehaviour
+{
+    // Set name to startroom to be able to find this room
+    void Start()
+    {
+		gameObject.name = "startRoom";
+    }
+}
