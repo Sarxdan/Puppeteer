ab46860fc71ffa50f8bcd54a384ad6fae846772a
Anton Jonsson <jonano-7@student.ltu.se>
2019-04-30 10:35:49 +0200

    Fixed Froggo and added new struct for sending position and rotation over network

---
+++ Assets/Resources/Scripts/Level/TransformStruct.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct TransformStruct
+{
+	public Vector3 Position;
+	public Quaternion Rotation;
+
+	public TransformStruct(Vector3 pos, Quaternion rot)
+	{
+		Position = pos;
+		Rotation = rot;
+	}
+}
