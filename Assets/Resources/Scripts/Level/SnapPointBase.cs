14de588c29ec9cf275adc10628fafdd500a038c4
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-05-02 15:51:08 +0200

    working trap grab tool.

--- Assets/Resources/Scripts/Level/SnapPointBase.cs
+++ Assets/Resources/Scripts/Level/SnapPointBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public abstract class SnapPointBase : NetworkBehaviour
+public abstract class SnapPointBase : MonoBehaviour
 {
     // THIS IS SUPPOSED TO BE EMPTY, IF QUESTIONS ASK BAMSE
 }
