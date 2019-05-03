f82ad68e3ef17ddc442dfd35e15fd42f8e80df43
Sanank-7 <Sanank-7@student.ltu.se>
2019-05-03 13:54:44 +0200

    Started on the component for fake item. Still in heavy experimenting

---
+++ Assets/Resources/Scripts/Traps/FakeItem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FakeItem : Interactable
+{
+    public GameObject[] Models; //[0] is default model
+    public uint Damage;
+    public float DestroyTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Test");
+    }
+
+    public IEnumerator Test()
+    {
+        yield return new WaitForSeconds(5);
+        Models[0].SetActive(false);
+        Models[Random.Range(1, Models.Length)].SetActive(true);
+    }
+
+    public override void OnInteractBegin(GameObject interactor)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5);
+
+        foreach (Collider hit in hitColliders)
+        {
+            if (hit.gameObject.tag == "Player")
+            {
+                hit.GetComponent<HealthComponent>().Damage(Damage);
+            }
+        }
+
+        StartCoroutine("DestroyTimer");
+    }
+
+    public IEnumerator DestroyTimer()
+    {
+        yield return new WaitForSeconds(DestroyTime);
+
+        if (gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnInteractEnd(GameObject interactor)
+    {
+    }
+}
