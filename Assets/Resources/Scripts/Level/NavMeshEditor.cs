9ce7c5ab7e49ee7403870fc188499b449df261a7
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-05-06 19:14:06 +0200

    Added missing editors (oops)
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

---
+++ Assets/Resources/Scripts/Level/NavMeshEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*
+ * AUTHOR:
+ * Ludvig Björk Förare
+ * 
+ * DESCRIPTION:
+ * An editor mod that adds buttons for baking/clearing the data from a navmesh component
+ *
+ * CODE REVIEWED BY:
+ * 
+ */
+
+[CustomEditor(typeof(NavMesh))]
+public class NavMeshEditor : Editor
+{
+    private NavMesh script;
+    
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        script = (NavMesh)target;
+        EditorGUI.BeginDisabledGroup(script.faces != null && script.faces.Length > 0);
+        if(GUILayout.Button("Bake navmesh"))
+        {
+            script.BakeNavmesh();
+        }
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUI.BeginDisabledGroup(script.faces == null || script.faces.Length == 0);
+        if(GUILayout.Button("Clear navmesh"))
+        {
+            script.ClearNavmesh();
+        }
+        EditorGUI.EndDisabledGroup();
+
+    }
+}
