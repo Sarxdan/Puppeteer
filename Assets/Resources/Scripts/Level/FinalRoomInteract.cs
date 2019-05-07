53b9f5c01da585830b4ba3d5592cffc7e4ce9a61
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-05-07 11:39:56 +0200

    Work on Final room and other small changes.

---
+++ Assets/Resources/Scripts/Level/FinalRoomInteract.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalRoomInteract : Interactable
+{
+	public int TimeLeft;
+	public GameObject DoorToOpen;
+
+	public override void OnInteractBegin(GameObject interactor)
+	{
+		StartCoroutine("FinalCountDown");
+	}
+
+	public override void OnInteractEnd(GameObject interactor)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public IEnumerator FinalCountDown()
+	{
+		
+		while(true)
+		{
+			if (TimeLeft > 0)
+			{
+				TimeLeft -= 1;
+			}
+			else
+			{
+
+			}
+			yield return new WaitForSeconds(1);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
