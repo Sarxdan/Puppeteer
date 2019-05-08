bfbfec55a844784f37b75e50ea9cd54c5b0a97c5
Filip Renman <filren-7@student.ltu.se>
2019-05-08 10:06:31 +0200

    You can now press enter to join a lobby when the input field is selected

---
+++ Assets/Resources/Scripts/Menus/InputFieldPressButtonOnEnterPress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldPressButtonOnEnterPress : MonoBehaviour
+{
+    [Tooltip("The button that will be pressed when you press enter when the inputfield is selected")]
+    public Button ButtonPressedOnEnter;
+
+    private InputField field;
+
+    void Start()
+    {
+        field = gameObject.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        if (field.isFocused && field.text != "" && Input.GetKeyDown(KeyCode.Return))
+        {
+            ButtonPressedOnEnter.onClick.Invoke();
+        }
+    }
+}
