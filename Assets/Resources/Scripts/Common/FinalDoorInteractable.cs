4a12ce0c3f23464c1f0178144210ea540e7051f1
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-05-10 12:12:36 +0200

    Added finalroom Functionality

---
+++ Assets/Resources/Scripts/Common/FinalDoorInteractable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+/*
+* AUTHOR:
+* Benjamin "Boris" Vesterlund
+*
+* DESCRIPTION:
+* Point holding info for placing traps.
+*
+* CODE REVIEWED BY:
+*
+* CONTRIBUTORS:
+* 
+*/
+
+public class FinalDoorInteractable : Interactable
+{
+	public override void OnInteractBegin(GameObject interactor)
+	{
+		Debug.Log("Interact with Door.");
+		var button = GameObject.Find("FinalButton(Clone)");
+		var buttonScript = button.GetComponent<FinalRoomInteract>();
+		if (buttonScript.Opened)
+		{
+			Debug.Log("Interacting when door open.");
+			RpcTurnOff(interactor);
+		}
+	}
+
+	public override void OnInteractEnd(GameObject interactor)
+	{
+		Debug.Log("End");
+	}
+
+	[ClientRpc]
+	public void RpcTurnOff(GameObject interactor)
+	{
+		Destroy(interactor);
+		var playerList = GameObject.FindGameObjectsWithTag("Player");
+		var camera = playerList[0].GetComponentInChildren<Camera>();
+		camera.enabled = true;
+	}
+}
