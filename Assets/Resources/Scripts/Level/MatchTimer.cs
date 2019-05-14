2e1f406979c99a5f86132d73ef1091a0636d9444
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-05-14 16:42:07 +0200

    Work on puppeteer UI - buttons

--- Assets/Resources/Scripts/Level/MatchTimer.cs
+++ Assets/Resources/Scripts/Level/MatchTimer.cs
@@ -88,52 +88,52 @@ public class MatchTimer : NetworkBehaviour
 
             //Check if a team have won
 
-            //If a puppet have escaped and all other are dead, the puppets win
-            if (NumberOfPuppetsThatEscaped >= numberOfPuppetsAlive && NumberOfPuppetsThatEscaped != 0 && !gameEnded)
-            {
-                //End the game. Puppets wins
-                gameEnded = true;
-                RpcPuppetsWins(numberOfPuppetsAlive, Minutes, Seconds);
-                MatchLength = 0;
-                StartCoroutine("EndTimer");
-                StopCoroutine("Timer");
-            }
+            ////If a puppet have escaped and all other are dead, the puppets win
+            //if (NumberOfPuppetsThatEscaped >= numberOfPuppetsAlive && NumberOfPuppetsThatEscaped != 0 && !gameEnded)
+            //{
+            //    //End the game. Puppets wins
+            //    gameEnded = true;
+            //    RpcPuppetsWins(numberOfPuppetsAlive, Minutes, Seconds);
+            //    MatchLength = 0;
+            //    StartCoroutine("EndTimer");
+            //    StopCoroutine("Timer");
+            //}
 
-            //If no puppets are alive, the puppeteer wins
-            else if (numberOfPuppetsAlive <= 0 && !gameEnded)
-            {
-                //End the game. Puppeteer wins
-                gameEnded = true;
-                RpcPuppeteerWins(numberOfPuppetsAlive, Minutes, Seconds);
-                MatchLength = 0;
-                StartCoroutine("EndTimer");
-                StopCoroutine("Timer");
-            }
+            ////If no puppets are alive, the puppeteer wins
+            //else if (numberOfPuppetsAlive <= 0 && !gameEnded)
+            //{
+            //    //End the game. Puppeteer wins
+            //    gameEnded = true;
+            //    RpcPuppeteerWins(numberOfPuppetsAlive, Minutes, Seconds);
+            //    MatchLength = 0;
+            //    StartCoroutine("EndTimer");
+            //    StopCoroutine("Timer");
+            //}
 
-            numberOfPuppetsAlive = GameObject.FindGameObjectsWithTag("Player").Length;
+            //numberOfPuppetsAlive = GameObject.FindGameObjectsWithTag("Player").Length;
         }
 
-        //If the time runs out and one puppet have escaped. The puppets win
-        if (NumberOfPuppetsThatEscaped >= 1)
-        {
-            //End the game. Puppets wins
-            gameEnded = true;
-            RpcPuppetsWins(numberOfPuppetsAlive, Minutes, Seconds);
-            MatchLength = 0;
-            StartCoroutine("EndTimer");
-            StopCoroutine("Timer");
-        }
+        ////If the time runs out and one puppet have escaped. The puppets win
+        //if (NumberOfPuppetsThatEscaped >= 1)
+        //{
+        //    //End the game. Puppets wins
+        //    gameEnded = true;
+        //    RpcPuppetsWins(numberOfPuppetsAlive, Minutes, Seconds);
+        //    MatchLength = 0;
+        //    StartCoroutine("EndTimer");
+        //    StopCoroutine("Timer");
+        //}
 
-        //If the time runs out. The puppeteer wins
-        else
-        {
-            //End the game. Puppeteer wins
-            gameEnded = true;
-            RpcPuppeteerWins(numberOfPuppetsAlive, Minutes, Seconds);
-            MatchLength = 0;
-            StartCoroutine("EndTimer");
-            StopCoroutine("Timer");
-        }
+        ////If the time runs out. The puppeteer wins
+        //else
+        //{
+        //    //End the game. Puppeteer wins
+        //    gameEnded = true;
+        //    RpcPuppeteerWins(numberOfPuppetsAlive, Minutes, Seconds);
+        //    MatchLength = 0;
+        //    StartCoroutine("EndTimer");
+        //    StopCoroutine("Timer");
+        //}
 
     }
 
