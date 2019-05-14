1f478e35358edd13f2ebc4701b7eed168d23d29e
Anton Jonsson <jonano-7@student.ltu.se>
2019-05-14 17:18:20 +0200

    Added DecalHandler script. Not fully implemented into weapon yet.

---
+++ Assets/Resources/Scripts/Custom editors/DecalHandlerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DecalHandler))]
+public class DecalHandlerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+
+		DecalHandler script = (DecalHandler)target;
+
+		DrawDefaultInspector(); 
+
+		if (script.DecalDecay)
+		{
+			script.DecayTime = EditorGUILayout.FloatField("Decay Time", script.DecayTime);
+		}
+	}
+}
+#endif
---
+++ Assets/Resources/Scripts/Puppet/DecalHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * AUTHOR:
+ * Anton Jonsson
+ * 
+ * DESCRIPTION:
+ * Script used to spawn and despawn decals
+ * 
+ * CODE REVIEWED BY:
+ * 
+ * CONTRIBUTORS:
+ * Sandra Andersson
+ */
+
+public class DecalHandler : MonoBehaviour
+{
+	public int MaxDecalAmount = 50;
+	public bool DecalDecay = true;
+	[HideInInspector]
+	public float DecayTime = 10;
+	private Queue<GameObject> decalQueue = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (DecalDecay)
+		{
+			StartCoroutine("DecayDecals");
+		}
+    }
+
+	public void AddDecal(GameObject decal)
+	{
+		decalQueue.Enqueue(decal);
+
+		if (decalQueue.Count > MaxDecalAmount)
+		{
+			Destroy(decalQueue.Dequeue());
+		}
+	}
+
+	public IEnumerator DecayDecals()
+	{
+		while (true)
+		{
+			// Destroy first decal in queue
+			if (decalQueue.Count > 0)
+			{
+				Destroy(decalQueue.Dequeue());
+			}
+
+			yield return new WaitForSeconds(DecayTime);
+		}
+	}
+}
