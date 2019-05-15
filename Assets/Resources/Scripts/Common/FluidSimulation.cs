554a47b91474a77c90cac89be056cb3673df05e9
Anton Jonsson <jonano-7@student.ltu.se>
2019-05-15 12:13:23 +0200

    Cleaned an peer reviewed fluid simulation script

--- Assets/Resources/Scripts/Common/FluidSimulation.cs
+++ Assets/Resources/Scripts/Common/FluidSimulation.cs
@@ -8,39 +8,39 @@
  * Script used to simulate a fluid moving in a cylinder.
  * 
  * CODE REVIEWED BY:
- * 
+ * Philip Stenmark (15/05-2019)
  */
 
 public class FluidSimulation : MonoBehaviour
 {
+	// The actual fluid meshes that are rotated and scaled to create the effect.
 	public GameObject FluidTop;
 	public GameObject FluidBottom;
 
-	// Original scale and rotation values of the objects
+	// Original scale and rotation values of the objects.
 	private Vector3 topScale;
 	private Vector3 bottomScale;
 	private Vector3 topRotation;
 	private Vector3 bottomRotation;
 
-	private Rigidbody parentRigidBody;
+	// Height of object in Unity units. Might be tricky to find but found no way of automatically gathering it.
+	public float TopHeight;
+	public float BottomHeight;
 
-	// Height of objects in Unity units. Might be tricky to find but found no way of automatically gathering it.
-	public float topHeight;
-	public float bottomHeight;
-
-	// Degrees of tilt for fluid at standard scale
-	public float fluidTiltAmount;
+	// Degrees of tilt for fluid at standard scale.
+	public float FluidTiltAmount;
 
 	// Scale of top slanted fluid in local y-axis.
 	private float currentTopScale = 0.2f;
+	// Maximum allowed scale of top fluid object.
 	public float MaxTiltScale = 3;
 
 	// Liquid amounts used to calculate percentage of liquid left.
 	public float MaxLiquidAmount = 100;
-	public float CurrentLiquidAmount = 0;
+	public float CurrentLiquidAmount = 100;
 
-	public float sploshAmount = 0.007f;
-	public float sploshSlowdown = 0.7f;
+	public float SploshAmount = 0.007f;
+	public float SploshSlowdown = 0.7f;
 	private float rotateAcceleration = 0;
 	private float rotateSpeed = 0;
 
@@ -59,13 +59,11 @@ public class FluidSimulation : MonoBehaviour
 		bottomScale = FluidBottom.transform.localScale;
 		topRotation = FluidTop.transform.localEulerAngles;
 		bottomRotation = FluidBottom.transform.localEulerAngles;
-		parentRigidBody = GetComponentInParent<Rigidbody>();
 
 		UpdateRotationAndTilt();
 		UpdatePositionAndScale();
 	}
 
-	// Update is called once per frame
 	void FixedUpdate()
 	{
 		UpdateRotationAndTilt();
@@ -88,50 +86,33 @@ public class FluidSimulation : MonoBehaviour
 		while (angleDiff < -180)
 			angleDiff += 360;
 
-		rotateAcceleration += angleDiff * sploshAmount;
-		rotateAcceleration *= sploshSlowdown;
-
-		if (rotateAcceleration >= 1)
-		{
-			rotateAcceleration = 1;
-		}
-		else if (rotateAcceleration <= -1)
-		{
-			rotateAcceleration = -1;
-		}
+		rotateAcceleration += angleDiff * SploshAmount;
+		rotateAcceleration *= SploshSlowdown;
+		rotateAcceleration = Mathf.Clamp(rotateAcceleration, -1, 1);
 
 		rotateSpeed += rotateAcceleration;
 
-		if (parentRigidBody != null)
-		{
-			rotateSpeed += parentRigidBody.angularVelocity.y;
-		}
-		else
-		{
-			parentRigidBody = GetComponentInParent<Rigidbody>();
-		}
-
-
+		// Slow speed down when speed is moving away from target in order to prevent a perfect pendulum.
 		if (Mathf.Abs(rotateSpeed + angleDiff) < Mathf.Abs(rotateSpeed))
 		{
-			rotateSpeed *= sploshSlowdown;
+			rotateSpeed *= SploshSlowdown;
 		}
 
-		FluidTop.transform.localEulerAngles = new Vector3(0, FluidTop.transform.localEulerAngles.y + rotateSpeed, 0);
+		FluidTop.transform.localEulerAngles = new Vector3(0, FluidTop.transform.localEulerAngles.y + rotateSpeed + topRotation.y, 0);
 		FluidBottom.transform.localEulerAngles = new Vector3(0, FluidTop.transform.localEulerAngles.y + bottomRotation.y, 0);
 
-		// Calculate lentgh of lean vector to get amount of fluid tilt.
+		// Calculate length of lean vector to get amount of fluid tilt.
 		// When the length of the lean vector is 1, the fluid is tilted 90 degrees.
 		float fluidTiltAngle = Mathf.Asin(leanVector.magnitude);
-		currentTopScale = Mathf.Tan(fluidTiltAngle) * topScale.y / (Mathf.Deg2Rad * fluidTiltAmount);
+		currentTopScale = Mathf.Tan(fluidTiltAngle) * topScale.y / (Mathf.Deg2Rad * FluidTiltAmount);
 
 		if (currentTopScale > MaxTiltScale)
 		{
 			currentTopScale = MaxTiltScale;
 		}
-		else if (currentTopScale < 0.0001f || float.IsNaN(currentTopScale))
+		else if (currentTopScale < Mathf.Epsilon || float.IsNaN(currentTopScale))
 		{
-			currentTopScale = 0.0001f;
+			currentTopScale = Mathf.Epsilon;
 		}
 	}
 
@@ -139,13 +120,10 @@ public class FluidSimulation : MonoBehaviour
 	{
 		// Calculate how much the bottom fluid has to move out of the way to make room for the top fluid as percentage of original scale.
 		float splashOffset = (currentTopScale / topScale.y);
-		// Calculate what percentage of its original scale the bottom fluid needs to be to make room for top fluid.
-		float bottomScalePercentage = splashOffset * (topHeight / 2) / bottomHeight;
-		float currentBottomScale = bottomScale.y - bottomScalePercentage * bottomScale.y;
 		// Calculate how much the total liquid has to sink as percentage of original amount.
 		float amountOffset = CurrentLiquidAmount / MaxLiquidAmount;
 		// Update positions of Liquid parts using offsets.
-		FluidTop.transform.localPosition = new Vector3(0, amountOffset * bottomHeight, 0);
+		FluidTop.transform.localPosition = new Vector3(0, amountOffset * BottomHeight, 0);
 		// Update Scale of Liquid parts using offsets
 		if (amountOffset != 0)
 		{
@@ -153,8 +131,9 @@ public class FluidSimulation : MonoBehaviour
 		}
 		else
 		{
-			FluidTop.transform.localScale = new Vector3(topScale.x, 0.0001f, topScale.z);
+			// If there is no liquid left, scale of top should always be 0 (Or as close as possible)
+			FluidTop.transform.localScale = new Vector3(topScale.x, Mathf.Epsilon, topScale.z);
 		}
-		FluidBottom.transform.localScale = new Vector3(bottomScale.x, currentBottomScale + amountOffset * bottomScale.y - bottomScale.y, bottomScale.z);
+		FluidBottom.transform.localScale = new Vector3(bottomScale.x, amountOffset * bottomScale.y, bottomScale.z);
 	}
 }
