3fa2afdab5e2ceafb21e6d5519571df7099a691f
Anton Jonsson <jonano-7@student.ltu.se>
2019-05-15 18:05:28 +0200

    Added doggo prefab and Sniffer powerup that currently just shows locked/unlocked doors

---
+++ Assets/Resources/Scripts/Puppet/SnifferPower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnifferPower : PowerupBase
+{
+	public override void OnActivate()
+	{
+		foreach (var door in FindObjectsOfType<DoorComponent>())
+		{
+			foreach (var renderer in door.GetComponentsInChildren<Renderer>())
+			{
+				if (!door.Locked)
+				{
+					renderer.material.SetColor("Color_D2F3C594", Color.green);
+				}
+				else
+				{
+					renderer.material.SetColor("Color_D2F3C594", Color.red);
+				}
+				renderer.material.SetInt("Vector1_67A4DF5D", 1);
+			}
+		}
+	}
+
+	public override void OnComplete()
+	{
+		foreach (var door in FindObjectsOfType<DoorComponent>())
+		{
+			foreach (var renderer in door.GetComponentsInChildren<Renderer>())
+			{
+				renderer.material.SetInt("Vector1_67A4DF5D", 0);
+			}
+		}
+	}
+}
