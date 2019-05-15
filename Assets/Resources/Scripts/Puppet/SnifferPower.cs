0cb956555c276ac2d008bfeaf5b08246425e000c
Anton Jonsson <jonano-7@student.ltu.se>
2019-05-15 18:52:29 +0200

    Commented Sniffer powerup

--- Assets/Resources/Scripts/Puppet/SnifferPower.cs
+++ Assets/Resources/Scripts/Puppet/SnifferPower.cs
@@ -16,16 +16,18 @@ using UnityEngine;
 public class SnifferPower : PowerupBase
 {
 	// Int used to describe how much of the currently selected color should be blended into the doors colors
-	public int ColorAmount;
+	public int ColorAmount = 1;
 	public Color OpenColor = Color.green;
 	public Color LockedColor = Color.red;
 
 	public override void OnActivate()
 	{
+		// Go through each door and update the color of each shader in the door
 		foreach (var door in FindObjectsOfType<DoorComponent>())
 		{
 			foreach (var renderer in door.GetComponentsInChildren<Renderer>())
 			{
+				// Choose color depending on if the door is locked or not
 				if (!door.Locked)
 				{
 					renderer.material.SetColor("Color_D2F3C594", OpenColor);
@@ -34,6 +36,7 @@ public class SnifferPower : PowerupBase
 				{
 					renderer.material.SetColor("Color_D2F3C594", LockedColor);
 				}
+				// For all renderers in door, start blending the colors to the chosen amount
 				renderer.material.SetInt("Vector1_67A4DF5D", ColorAmount);
 			}
 		}
@@ -41,6 +44,7 @@ public class SnifferPower : PowerupBase
 
 	public override void OnComplete()
 	{
+		// Set the blending amount to 0 for all renderers to stop adding color.
 		foreach (var door in FindObjectsOfType<DoorComponent>())
 		{
 			foreach (var renderer in door.GetComponentsInChildren<Renderer>())
