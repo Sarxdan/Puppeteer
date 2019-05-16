053f302807e04ddd692e2f97dc147990f56cd2ba
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-05-16 15:01:28 +0200

    fixing prefabs

--- Assets/Resources/Scripts/Common/EnemySpawner.cs
+++ Assets/Resources/Scripts/Common/EnemySpawner.cs
@@ -23,7 +23,7 @@ public class EnemySpawner : NetworkBehaviour
 {
     public GameObject EnemyPrefab;
     public int MaxEnemyCount;
-
+	 
     public int MinDelay = 5;
     public int MaxDelay = 10;
     public bool TankSpawner;
@@ -43,8 +43,7 @@ public class EnemySpawner : NetworkBehaviour
         finalRoomDummy = GameObject.Find("DummySpawner").GetComponent<EnemySpawner>();
         if(finalRoomDummy == gameObject) return;
         
-        if(MinionContainerObject == null)
-        {
+        if(MinionContainerObject == null){
             MinionContainerObject = GameObject.Find("MinionContainer").transform;
         }
         snapFunctionality = GetComponent<SnapFunctionality>();
@@ -53,16 +52,15 @@ public class EnemySpawner : NetworkBehaviour
         HealthComponent hpComponent = GetComponent<HealthComponent>();
         hpComponent.AddDeathAction(OnDeath);
         hpComponent.AddOnDamageAction(CmdOnTakeDamage);
+
     }
 
 
     //Spawn is a modified Update with a set amount of time (SpawnRate) between runs
     private IEnumerator Spawn()
     { 
-        while(true)
-        {
-            if(isServer && snapFunctionality.Placed)
-            {
+        while(true){
+            if(isServer && snapFunctionality.Placed){
                 //Check if max amount of enemies has been reached
                 if (LocalMinions.Count < MaxEnemyCount && MaxEnemyCount > 0)
                 {
@@ -83,6 +81,7 @@ public class EnemySpawner : NetworkBehaviour
                     {
                         gameObject.SetActive(false);
                     }
+    
                 }
             }
             yield return new WaitForSeconds(Random.Range(MinDelay, MaxDelay));
@@ -93,19 +92,15 @@ public class EnemySpawner : NetworkBehaviour
     
 
     [Command]
-    public void CmdOnTakeDamage()
-    {
-        foreach(StateMachine minion in LocalMinions)
-        {
+    public void CmdOnTakeDamage(){
+        foreach(StateMachine minion in LocalMinions){
             if(minion.TargetEntity == null)
                 minion.SetState(new ReturnToSpawnerState(minion));
         }
     }
 
-    public void OnDeath()
-    {
-        foreach(StateMachine minion in LocalMinions)
-        {
+    public void OnDeath(){
+        foreach(StateMachine minion in LocalMinions){
             minion.Spawner = null;
         }
         Destroy(gameObject);
