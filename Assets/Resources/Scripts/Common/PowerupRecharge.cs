e6bc8e63f64d6e3bfb4e50751e3416cc9958d51f
sanank-7 <sanank-7@student.ltu.se>
2019-05-16 13:57:45 +0200

    Started on updating the power-up prefab

--- Assets/Resources/Scripts/Common/PowerupRecharge.cs
+++ Assets/Resources/Scripts/Common/PowerupRecharge.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 /*
@@ -12,19 +13,24 @@ using UnityEngine;
  * 
  * CODE REVIEWED BY:
  * Anton Jonsson (24/4)
- * 
+ *
+ * CONTRIBUTORS:
+ * Sandra Andersson (Implemented with animation)
+ *
  */
 public class PowerupRecharge : Interactable
 {
     public override void OnInteractBegin(GameObject interactor)
     {
         var power = interactor.GetComponent<PowerupBase>();
+        Animator anim = GetComponentInChildren<Animator>();
 
         if (power.isServer && power.isLocalPlayer)
         {
             // attempt to pickup recharge
             if (power != null && power.PercentageLeft == 0.0f)
             {
+                anim.SetTrigger("Consume");
                 power.Charged = true;
             }
         }
@@ -32,11 +38,10 @@ public class PowerupRecharge : Interactable
         {
             if (power != null)
             {
+                anim.SetTrigger("Consume");
                 power.RpcBoostPowerup();
             }
         }
-
-        Destroy(gameObject);
     }
 
     public override void OnInteractEnd(GameObject interactor)
@@ -46,8 +51,12 @@ public class PowerupRecharge : Interactable
 
     public override void OnRaycastEnter(GameObject interactor)
     {
-        if(!interactor.GetComponent<PowerupBase>().Charged)
+        if (!interactor.GetComponent<PowerupBase>().Charged)
             ShowTooltip(interactor);
     }
 
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
 }
