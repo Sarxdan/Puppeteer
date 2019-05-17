286e12021c7b9b3f451e6b324258f09fc5558b19
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-05-17 10:33:49 +0200

    Fixed misaligned door nodes Fixed so StartWeaponSpawner tries to spawn for 10sec
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

--- Assets/Resources/Scripts/Puppeteer/StartWeaponSpawner.cs
+++ Assets/Resources/Scripts/Puppeteer/StartWeaponSpawner.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
+/*
+* AUTHOR:
+* Ludvig "Kät" Björk Förare
+*
+* DESCRIPTION:
+* Script that spawns in weapons for players joining the game. Runs for 10 sec then destroys itself
+*
+* CODE REVIEWED BY:
+*
+* CONTRIBUTORS: 
+*
+*/
 public class StartWeaponSpawner : NetworkBehaviour
 {
-    // Start is called before the first frame update
     public GameObject StartWeapon;
     void Start()
     {
-        if(isServer)
+        if(!isServer)
+            Destroy(this);
+        else
+            Destroy(this, 10);
+    }
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject player in players)
         {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            foreach(GameObject player in players)
+            if(player.GetComponent<PlayerController>().CurrentWeapon == null)
             {
                 GameObject spawnedWeapon = Instantiate(StartWeapon, Vector3.zero, transform.rotation);
                 NetworkServer.Spawn(spawnedWeapon);
