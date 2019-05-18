9b5277a0e5ac66c9f49d72a284fac3d45afa8f5a
Ludvig Björk Förare <ludfra-7@student.ltu.se>
2019-05-18 23:10:14 +0200

    Added painting image randomizer
    Signed-off-by: Ludvig Björk Förare <ludfra-7@student.ltu.se>

---
+++ Assets/PaintingRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * AUTHOR:
+ * Ludvig "Kät" Björk Förare
+ * 
+ * DESCRIPTION:
+ * Script to randomize painting images
+ * 
+ * CODE REVIEWED BY:
+ * 
+ */
+public class PaintingRandomizer : MonoBehaviour
+{
+    public int NumberOfPictures;
+    private Renderer rend;
+    void Start()
+    {
+        rend = transform.Find("Canvas").GetComponent<Renderer>();
+        rend.material.SetInt("_Picture_Select", Random.Range(0, NumberOfPictures - 1));
+    }
+}
