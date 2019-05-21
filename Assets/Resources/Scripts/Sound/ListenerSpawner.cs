288a1c7cb51ddbdd835d08615a61c5d7dfdf0954
Anton Jonsson <jonano-7@student.ltu.se>
2019-05-21 19:25:24 +0200

    Adding sound listeners in script now

---
+++ Assets/Resources/Scripts/Sound/ListenerSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ListenerSpawner : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (isLocalPlayer)
+		{
+			GetComponentInChildren<Camera>().gameObject.AddComponent<FMODUnity.StudioListener>();
+		}
+    }
+}
